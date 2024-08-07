adbd8e10ec62669a1fa549fe991ee190ebd23986
egor <Egor78396@gmail.com>
2024-08-07 15:10:27 +0300

    user-database-service configured

---
+++ SocialWeb/user-database-service/src/main/java/org/example/userdatabaseservice/UserDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.userdatabaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDatabaseServiceApplication.class, args);
+	}
+
+}
---
+++ SocialWeb/user-database-service/src/test/java/org/example/userdatabaseservice/UserDatabaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.userdatabaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDatabaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
