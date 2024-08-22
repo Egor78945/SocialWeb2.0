3625e977480ff62220095d904dd0473622a8975f
egor <Egor78396@gmail.com>
2024-08-22 14:13:43 +0300

    some changes

--- SocialWeb/friendship-service/src/main/java/org/example/friendshipservice/FriendshipServiceApplication.java
+++ SocialWeb/friendship-service/src/main/java/org/example/friendshipservice/FriendshipServiceApplication.java
@@ -3,6 +3,7 @@ package org.example.friendshipservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//fffffffffffff
 @SpringBootApplication
 public class FriendshipServiceApplication {
 
