a470a540112bd2704cb6fdfe54ce1946096663b2
egor <Egor78396@gmail.com>
2024-09-22 13:18:22 +0300

    message-s3-service(created)

---
+++ SocialWeb/message-s3-service/src/main/java/org/example/messages3service/MessageS3ServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.messages3service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageS3ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageS3ServiceApplication.class, args);
+    }
+
+}
---
+++ SocialWeb/message-s3-service/src/test/java/org/example/messages3service/MessageS3ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.messages3service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageS3ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
