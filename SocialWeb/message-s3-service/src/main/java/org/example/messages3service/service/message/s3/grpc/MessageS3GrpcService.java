7b4060c81f43e063a5495ec7bc653f0c5384d726
egor <Egor78396@gmail.com>
2024-10-04 16:03:32 +0300

    message-s3-service(MessageS3GrpcService)

--- SocialWeb/message-s3-service/src/main/java/org/example/messages3service/service/message/s3/grpc/MessageS3GrpcService.java
+++ SocialWeb/message-s3-service/src/main/java/org/example/messages3service/service/message/s3/grpc/MessageS3GrpcService.java
@@ -1,4 +1,11 @@
 package org.example.messages3service.service.message.s3.grpc;
 
-public class MessageS3GrpcService {
+import com.example.grpc.message.s3.MessageS3ServiceGrpc;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MessageS3GrpcService extends MessageS3ServiceGrpc.MessageS3ServiceImplBase {
+
 }
